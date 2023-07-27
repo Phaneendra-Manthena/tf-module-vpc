b23018226d1d8b36d27af483365506fbb74b5624
phaneendra <phani.manthena27@gmail.com>
2023-07-27 16:44:44 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -2,37 +2,37 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-vpc"}
-    )
+    { Name = "${var.env}-vpc" }
+  )
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnets_cidr)
-  vpc_id = aws_vpc.main.id
+  count      = length(var.public_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets_cidr[count.index]
   tags = merge(
-  local.common_tags,
-{Name = "${var.env}-public-subnet-${count.index+1}"}
-)
+    local.common_tags,
+    { Name = "${var.env}-public-subnet-${count.index + 1}" }
+  )
 }
 resource "aws_subnet" "private" {
-  count = length(var.private_subnets_cidr)
-  vpc_id = aws_vpc.main.id
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets_cidr[count.index]
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-private-subnet-${count.index+1}"}
+    { Name = "${var.env}-private-subnet-${count.index + 1}" }
   )
 }
 
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
-  peer_vpc_id = var.default_vpc_id
-  vpc_id      = aws_vpc.main.id
-  auto_accept = true
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept   = true
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-peering"}
+    { Name = "${var.env}-peering" }
   )
 }
 
@@ -41,7 +41,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-igw"}
+    { Name = "${var.env}-igw" }
   )
 }
 
@@ -52,17 +52,17 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.igw.id
   }
   route {
-    cidr_block = data.aws_vpc.default.cidr_block
+    cidr_block                = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-public-route-table"}
+    { Name = "${var.env}-public-route-table" }
   )
 }
 resource "aws_route_table_association" "public-rt-association" {
-  count = length(aws_subnet.public)
-  subnet_id = "aws_subnet_public.*.id[count.index]"
+  count          = length(aws_subnet.public)
+  subnet_id      = "aws_subnet_public.*.id[count.index]"
   route_table_id = "aws_route_table.public.id"
 }
 
@@ -71,10 +71,10 @@ resource "aws_eip" "ngw-eip" {
 
 resource "aws_nat_gateway" "ngw" {
   allocation_id = "aws_eip.ngw-eip.id"
-  subnet_id = aws_subnet.public.*.id[0]
+  subnet_id     = aws_subnet.public.*.id[0]
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-ngw"}
+    { Name = "${var.env}-ngw" }
   )
   depends_on = [aws_internet_gateway.igw]
 }
@@ -82,28 +82,28 @@ resource "aws_nat_gateway" "ngw" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
   route {
-    cidr_block = data.aws_vpc.default.cidr_block
+    cidr_block                = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-public-route-table"}
+    { Name = "${var.env}-public-route-table" }
   )
 }
 
 resource "aws_route_table_association" "private-rt-association" {
-  count = length(aws_subnet.private)
-  subnet_id = "aws_subnet_private.*.id[count.index]"
+  count          = length(aws_subnet.private)
+  subnet_id      = "aws_subnet_private.*.id[count.index]"
   route_table_id = "aws_route_table.private.id"
 }
 
 resource "aws_route" "default" {
-  route_table_id = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block = var.cidr_block
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
