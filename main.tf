b3c690c79e3a77d31816a98c2151a804e026ae81
phaneendra <phani.manthena27@gmail.com>
2023-07-27 11:24:59 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ resource "aws_vpc" "main" {
     {Name = "${var.env}-vpc"}
     )
 }
+
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id = "var.default_vpc_id"
+  vpc_id      = aws_vpc.main.id
+  auto_accept = true
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-peering"}
+  )
+}
+
--- vars.tf
+++ vars.tf
@@ -1,2 +1,3 @@
 variable "cidr_block" {}
 variable "env" {}
+variable "default_vpc_id" {}
